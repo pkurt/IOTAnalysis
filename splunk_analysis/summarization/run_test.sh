969e912263ae04a134a411fce2dc1f083cc90bce
pkurt <pkurt@cern.ch>
2015-08-04 23:25:43 -0700

    Cleaning up duplicate pythonUtils in summary directories

--- splunk_analysis/summarization/run_test.sh
+++ splunk_analysis/summarization/run_test.sh
@@ -1,3 +1,3 @@
-export PYTHONPATH=/Users/pelinkurtgarberson/Desktop/sdk_python/splunk-sdk-python-master/
+export PYTHONPATH=/Users/pelinkurtgarberson/Desktop/sdk_python/splunk-sdk-python-master/:/Users/pelinkurtgarberson/IOTAnalysis
 
 python makeSummaries.py 20 '2014-01-01T00:00:00' '2014-12-31T23:00:00' daily
