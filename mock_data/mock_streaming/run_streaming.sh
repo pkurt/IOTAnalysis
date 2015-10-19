f63b1a19c129cb01940583940ed7c14dff371b4f
pkurt <pkurt@cern.ch>
2015-10-18 22:49:09 -0700

    added more arguemnts to run multiple simulations with different configurations

--- mock_data/mock_streaming/run_streaming.sh
+++ mock_data/mock_streaming/run_streaming.sh
@@ -1,3 +1,6 @@
 #python stream_data.py 2014-01-01 2015-01-01 121 bigOutFileSeasonal.json
 
-python stream_data.py --startTime 2014-01-01 --endTime 2015-01-01 --timeStepSeconds 61 --outFileName SingleValueDataFormat.csv --deviceType Thermostat --houseId 500
+python stream_data.py --startTime 2014-01-01 --endTime 2015-01-01 --timeStepSeconds 61 --outFileName SingleValueDataFormat_id68.csv --deviceType Thermostat --houseId 500 --hvac_id 68 --insulation 150 --avgBaselineTemp 60 --seasonalWeatherVariation 0.4
+python stream_data.py --startTime 2014-01-01 --endTime 2015-01-01 --timeStepSeconds 61 --outFileName SingleValueDataFormat_id69.csv --deviceType Thermostat --houseId 500 --hvac_id 69 --insulation 600 --avgBaselineTemp 60 --seasonalWeatherVariation 0.4
+python stream_data.py --startTime 2014-01-01 --endTime 2015-01-01 --timeStepSeconds 61 --outFileName SingleValueDataFormat_id70.csv --deviceType Thermostat --houseId 500 --hvac_id 70 --insulation 300 --avgBaselineTemp 60 --seasonalWeatherVariation 0.6
+python stream_data.py --startTime 2014-01-01 --endTime 2015-01-01 --timeStepSeconds 61 --outFileName SingleValueDataFormat_id71.csv --deviceType Thermostat --houseId 500 --hvac_id 71 --insulation 300 --avgBaselineTemp 60 --seasonalWeatherVariation 0.2
