cc806a24de6a4b8c1013e8b00e8311e43074079e
pkurt <pkurt@cern.ch>
2015-10-20 11:24:57 -0700

    aggregated results for multiple thermostats

--- splunk_analysis/run_test_daily.sh
+++ splunk_analysis/run_test_daily.sh
@@ -2,4 +2,8 @@ export PYTHONPATH=/Users/pelinkurtgarberson/Desktop/sdk_python/splunk-sdk-python
 
 #python thermostat_data_analysis_daily.py 30 '2014-01-01T00:00:00' '2015-01-01T00:00:00' 'dailyAggregatedSummary'
 #python thermostat_data_analysis_daily.py 30 '2014-01-01T00:00:00' '2015-01-01T00:00:00' 'dailySummaryTest3'
-python thermostat_data_analysis_daily.py 56 '2014-01-01T00:00:00' '2015-01-01T00:00:00' 'dailyAggSummaryNew'
+
+
+#python thermostat_data_analysis_daily.py 56 '2014-01-01T00:00:00' '2015-01-01T00:00:00' 'dailyAggSummaryNew'
+python thermostat_data_analysis_daily.py '[68,69,70,71]' '2014-01-01T00:00:00' '2015-01-01T00:00:00' 'dailyAggSummary'
+
--- splunk_analysis/thermostat_data_analysis_daily.py
+++ splunk_analysis/thermostat_data_analysis_daily.py
@@ -22,6 +22,22 @@ import io
 from email.mime.text import MIMEText
 import collections
 
+def convertStringToList(myStr, dtype):
+    ''' Assume we have been given a string of a list.
+        Extract and return the list
+        dtype: this will be the type of the list elements
+        '''
+    assert myStr[0] == '['
+    assert myStr[-1] == ']'
+    ### Convert string of '[1,2,3]' to '1,2,3'
+    contents = myStr[1:-1]
+    ### Now split string into list ('1,2,3' => ['1', '2', '3'])
+    myStringList = contents.split(',')
+    ### Now convert to proper data type
+    outputList = [dtype(ele) for ele in myStringList]
+    return outputList
+
+
 def filterData(dataDF):
     ''' Suggested filter: remove days with no runtime, such as
         avgDuration < 0.000001 '''
@@ -33,54 +49,44 @@ def main (argv):
     start_time = time.time()
     print 'Start of code'
     assert len(argv) == 5
-    thermostatId = int(argv[1])
+    #thermostatId = int(argv[1])
+    thermostatIdList = convertStringToList(argv[1], dtype=int)
+    print 'thermostatIdList is ', thermostatIdList
     StartTime = argv[2]
     EndTime = argv[3]
     DataType = argv[4]
 
-    print 'thermostatId = ', thermostatId, ', startTime = ', StartTime, ', endTime = ', EndTime, ', dataType = ', DataType
+    print 'thermostatId = ', thermostatIdList, ', startTime = ', StartTime, ', endTime = ', EndTime, ', dataType = ', DataType
 
-    searchReader = doSplunkSearch(thermostatId, StartTime, EndTime, DataType)
+    searchReader = doSplunkSearch(thermostatIdList, StartTime, EndTime, DataType)
     #print 'now getPandasDF'
-    sys.stdout.flush()
     myDataDF = getPandasDF(searchReader)
-    print 'after filter, dataDF: ', myDataDF
-    sys.stdout.flush()
-#    print 'now getRunPeriodDF'
-#    runPeriodDF = getRunPeriodDF(myDataDF)
-    #print 'runPeriodDF is ', runPeriodDF
-    dashboard_performance_plots(myDataDF)
-    #checkForAlert(myDataDF, runPeriodDF)
-   # checkForAlert(myDataDF)
 
+    #### Now clean any weird formats from data
+    #myDataDF = cleanData(myDataDF)
+    #myDataDF['timeStamp'] = pd.to_datetime(myDataDF['timeStamp'], format='%Y-%m-%d %H:%M:%S')
     
-    #print 'runPeriodDF: ', runPeriodDF
+    #### Now split into individual thermostats and do analysis:
+    myIDs = np.unique(myDataDF['id'].values)
+    for myID in myIDs:
+        print 'Do analysis for thermostat ', myID
+        #### Get dataframe for this ID:
+        thisTIDDF = myDataDF[myDataDF['id'] == myID]
+        print 'now getRunPeriodDF'
+        sys.stdout.flush()
+
+        #### Make plots for this thermostat
+        if len(thisTIDDF.index)> 0:
+            sys.stdout.flush()
+            dashboard_performance_plots(thisTIDDF, myID)
+            #checkForAlert(myDataDF, myID)
+
     runtime = time.time() - start_time
     sys.stdout.flush()
     print 'It took ', str(runtime), ' to run'
     sys.stdout.flush()
 
 
-#def getRunPeriodDF(myDataDF):
-#    listOfRunPeriods = []
-#    previousMode = 0
-#    eventsInThisRun = []
-#    print 'getRunPeriodDF from myDataDF: ', myDataDF
-#    for thisIdx, thisEvent in myDataDF.iterrows():
-#        isRunning = thisEvent['avgRunningMode']
-#        if previousMode == 1 and isRunning == False:
-#            runParameters = getRunParameters(eventsInThisRun)
-#            endRunTime = thisEvent['timeStamp']
-#            thisRunPeriod = {'beginRunTime': runParameters['beginRunTime'], 'endRunTime': runParameters['endRunTime'],'duration': runParameters['duration'], 'performance': runParameters['performance']}
-#            listOfRunPeriods.append(thisRunPeriod)
-#            eventsInThisRun = []
-#        if isRunning:
-#            eventsInThisRun.append(thisEvent)
-#        previousMode = thisEvent['avgRunningMode']
-#    print 'now make runPeriodDF from listOfRunPeriods: ', listOfRunPeriods
-#    
-#    runPeriodDF = pd.DataFrame(listOfRunPeriods)
-#    return runPeriodDF
 
 def getPandasDF(searchReader):
     #print 'getPandasDF'
@@ -90,10 +96,10 @@ def getPandasDF(searchReader):
 
 
     lineNum = 0
-    print 'searchReader has type: ', type(searchReader)
+    #print 'searchReader has type: ', type(searchReaderF)
     for line in searchReader:
         #print 'line: ', line
-        print ', '.join(line)
+        #print ', '.join(line)
         myData.append(line)
         lineNum += 1
         #print 'this row is: ', thisDict
@@ -104,41 +110,41 @@ def getPandasDF(searchReader):
             'avgOutsideTemp', 'minOutsideTemp', 'avgSetPoint', 'avgRunningMode', 'avgPerformance', 'avgDuration', 'numCycles']
     myDataDF[varsToBeFloats] = myDataDF[varsToBeFloats].astype(float)
     myDataDF['timeStamp'] = pd.to_datetime(myDataDF['timeStamp'])
-    print 'sort data in getPandasDF'
+    #print 'sort data in getPandasDF'
     sys.stdout.flush()
     myDataDF = myDataDF.sort('timeStamp')
-    print 'Full pandas data = ', myDataDF
+    #print 'Full pandas data = ', myDataDF
     sys.stdout.flush()
     return myDataDF
 
 #### Follow the example here: http://dev.splunk.com/view/python-sdk/SP-CAAAER5#reader
-def doSplunkSearch(thermostatId, StartTime, EndTime, DataType):
+def doSplunkSearch(thermostatIdList, StartTime, EndTime, DataType):
         
     #### do splunk search
-    #### output = splunkSearch(thermostatId, StartTime, EndTime)
     #### Organize into pandas dataFrame, myDataDF
-    service = client.connect(host='localhost', port=8089, username='admin', password='Pg18december', sourcetype='csv')
+    service = client.connect(host='localhost', port=8089, username='admin', password='Pg18december')
+    idSelectionRequirements = []
+    for myID in thermostatIdList:
+        thisIDStr = 'id='+str(myID)
+        idSelectionRequirements.append(thisIDStr)
+    idSelectionStr = '('+(' OR '.join(idSelectionRequirements))+')'
     #print 'got service, now make job'
     kwargs_oneshot={"earliest_time": StartTime,
                     "latest_time": EndTime,
                     "count": 0}
 
-    jobSearchString= "search id="+str(thermostatId) + " dataType="+str(DataType)+" | sort _time |"+\
-                      " table timeStamp maxInsideTemp avgInsideTemp minInsideTemp maxOutsideTemp avgOutsideTemp minOutsideTemp avgSetPoint avgRunningMode avgPerformance avgDuration numCycles"
-    #    jobSearchString= "search id="+str(thermostatId)+ "sourcetype="+str(csv) + " dataType="+str(DataType)+" | sort _time |"+\
-    #                  " table timeStamp maxInsideTemp avgInsideTemp minInsideTemp maxOutsideTemp avgOutsideTemp minOutsideTemp avgSetPoint avgRunningMode avgPerformance avgDuration numCycles"
+    jobSearchString= "search "+idSelectionStr+ " dataType="+str(DataType)+" | sort 0 id, _time |"+\
+                      " table id, timeStamp maxInsideTemp avgInsideTemp minInsideTemp maxOutsideTemp avgOutsideTemp minOutsideTemp avgSetPoint avgRunningMode avgPerformance avgDuration numCycles"
     job_results = service.jobs.oneshot(jobSearchString, **kwargs_oneshot)
-   # print 'jobSearchName: ', jobSearchString
     reader = results.ResultsReader(io.BufferedReader(responseReaderWrapper.ResponseReaderWrapper(job_results)))
-    #reader = results.ResultsReader(job_results)
     return reader
 
 
-def dashboard_performance_plots(myDataDF):
+def dashboard_performance_plots(myDataDF, myID):
     #print 'Now make plots for ', myDataDF
     xKey='timeStamp'
-    myPdf = PdfPages('AggregatedResultsFromId56.pdf')
-    
+    myPdf = PdfPages('images/plots_for_ID' + str(myID) + '_AggregatedResults.pdf')
+
     # draw avgTemperatures
     chartOptionsDict = collections.OrderedDict([
                         ('maxInsideTemp', {'color': 'mediumturquoise', 'style': '--', 'desc': 'DayMaxInsideTemp', 'linewidth':6}),
@@ -203,32 +209,6 @@ def dashboard_performance_plots(myDataDF):
     myPdf.close()
 
 
-
-#def getRunParameters(eventsInThisRun):
-   #def getPerformance(insideTemps, outsideTemps, duration):
-        #avgTempDiff = np.mean([outTemp - inTemp for inTemp, outTemp in zip(insideTemps, outsideTemps)])
-        #if(duration > 0.0001):
-            #performance = math.sqrt(max(0.01, avgTempDiff)) * (max(insideTemps) - min(insideTemps)) / duration
-        #else:
-            #performance = 0
-        #return performance
-    
-    #runParameters = {}
-    #runParameters['beginRunTime'] = eventsInThisRun[0]['timeStamp']
-    #runParameters['endRunTime'] = eventsInThisRun[-1]['timeStamp']
-    #runParameters['duration'] = (eventsInThisRun[-1]['timeStamp'] -
-    #                             eventsInThisRun[0]['timeStamp']).total_seconds() / 60.
-    #insideTemps = [event['avgInsideTemp'] for event in eventsInThisRun]
-    #outsideTemps = [event['avgOutsideTemp'] for event in eventsInThisRun]
-    #runParameters['performance'] = getPerformance(insideTemps, outsideTemps, runParameters['duration'])
-    #return runParameters
-
-
-
-
-
-
-#def checkForAlert(myDataDF, runPeriodDF):
 def checkForAlert(myDataDF):
     print 'checkforAlert'
     #myEmail = 'pelin.kurt.4d@gmail.com'
